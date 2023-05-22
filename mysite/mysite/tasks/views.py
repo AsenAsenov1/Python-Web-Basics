cc4346ee9a92331f8bc42061f61dde8c684d0224
Asen Asenov <asen991@gmail.com>
2023-05-22 16:02:46 +0300

    Upload: Project

---
+++ mysite/mysite/tasks/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Task
+
+
+def index(request):
+    tasks_list = Task.objects.all()
+    context = {'tasks_list': tasks_list}
+    return render(request, 'tasks/index.html', context)
